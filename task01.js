af2f50e854b394215b446762eb8a6ed8653df069
Jareeex <baswarafajar@gmail.com>
2024-06-11 06:06:28 +0700

    task01.js

--- task01.js
+++ task01.js
@@ -1,10 +1,16 @@
-function cekPalindrom (kata) {
-let kata2 = ""
-for(let i =kata.length; i>0;i--){
-  kata2 = kata2 + kata[i-1]
+// Mendefinisikan fungsi untuk memeriksa palindrom
+function cekPalindrom(kata) {
+    let kata2 = ""; // Variabel untuk menyimpan kata yang dibalik
+
+    // Melakukan iterasi mundur melalui setiap karakter kata
+    for (let i = kata.length; i > 0; i--) {
+        kata2 = kata2 + kata[i - 1]; // Membalikkan kata dengan cara menambahkan setiap karakter ke variabel kata2
+    }
+
+    // Memeriksa apakah kata yang dibalik sama dengan kata aslinya
+    if (kata2 === kata) {
+        console.log("Palindrom"); // Jika sama, kata adalah palindrom
+    }
 }
-if (kata2 === kata){
-  console.log("Polindrom")
-}
-}
-(cekPalindrom("malam"))
+
+cekPalindrom("malam"); // Memanggil fungsi dengan kata yang ingin diperiksa
